841e7f57026b76d495683e6305df8fe97e6dead1
temaors <artsiom.orsik@gmail.com>
2022-11-03 20:39:16 +0300

    added font awesome to views

--- Web_Autopark/Controllers/ComponentController.cs
+++ Web_Autopark/Controllers/ComponentController.cs
@@ -1,6 +1,20 @@
+using AutoparkDAL.Entities;
+using AutoparkDAL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Web_Autopark.Controllers;
 
-public class ComponentController
+public class ComponentController : Controller
 {
-    
+    public IRepository<Component> repo;
+
+    public ComponentController(IRepository<Component> rep)
+    {
+        repo = rep;
+    }
+    public async Task<IActionResult> Index()
+    {
+        var components = await repo.GetAll();
+        return View(components);
+    }
 }
